627b56df32d73fd49f3e964434bf7af543f6290b
P-Programist <p.programist.kg@gmail.com>
2021-08-25 11:12:55 +0600

    AWS Synchronization

--- configs/core.py
+++ configs/core.py
@@ -1,11 +1,14 @@
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
+from configs.constants import REDIS_PASSWORD
 
 storage = RedisStorage2(
     host='localhost',
-    db=1
+    db=1,
+    password=REDIS_PASSWORD
 )
 
 redworker = RedisStorage2(
     host='localhost',
-    db=2
+    db=2,
+    password=REDIS_PASSWORD
 )

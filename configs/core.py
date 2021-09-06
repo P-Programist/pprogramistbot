8554ba812b7edcd7d815fbdc583abee378ece9eb
BekturTurumbaev <turumbaevv@gmail.com>
2021-09-06 11:37:24 +0600

    Doc and 2 in 1 tables

--- configs/core.py
+++ configs/core.py
@@ -21,13 +21,15 @@ point = RedisStorage2(
     port = 6379, 
     password = REDIS_PASSWORD
 )
+'''Таблица point хранит в себе баллы собранные пользователем'''
 
-question = RedisStorage2(
+time_question = RedisStorage2(
     host='127.0.0.1',
     db=4,
     port = 6379, 
     password = REDIS_PASSWORD
 )
+'''Таблица time_question хранит в себе время в которое должен уложиться пользователь во время теста, также на каком вопросе находился пользователь'''
 
 check = RedisStorage2(
     host='127.0.0.1',
@@ -35,10 +37,4 @@ check = RedisStorage2(
     port = 6379, 
     password = REDIS_PASSWORD
 )
-
-time_trecker = RedisStorage2(
-    host='127.0.0.1',
-    db=6,
-    port = 6379, 
-    password = REDIS_PASSWORD
-)
+'''Таблица check хранит в себе ответы пользователя'''
